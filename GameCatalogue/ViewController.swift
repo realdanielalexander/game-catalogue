af005f7467531008f4a15ffa6a7b13432a3cc936
Daniel Alexander <realdanielalexander@gmail.com>
2020-09-05 10:08:04 +0700

    Initial Commit

---
+++ GameCatalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCatalogue
+//
+//  Created by Daniel Alexander on 05/09/20.
+//  Copyright © 2020 Daniel Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
